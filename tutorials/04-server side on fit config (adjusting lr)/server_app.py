b2d13981f0061626b3b3d0fdf8ee699e753a7048
unknown <maahaninir@gmail.com>
2025-05-23 00:13:39 +0330

    on fit config server side lr adjustment

---
+++ tutorials/04-server side on fit config (adjusting lr)/server_app.py
@@ -0,0 +1,62 @@
+"""myapp: A Flower / PyTorch app."""
+
+from flwr.common import Context, ndarrays_to_parameters, Metrics
+from flwr.server import ServerApp, ServerAppComponents, ServerConfig
+from flwr.server.strategy import FedAvg
+from myapp.task import Net, get_weights
+from typing import List, Tuple
+
+
+
+def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
+    """Aggregates metrics from an evaluate round."""
+    # Loop trough all metrics received compute accuracies x examples
+    accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
+    total_examples = sum(num_examples for num_examples, _ in metrics)
+    # Return weighted average accuracy
+    return {"accuracy": sum(accuracies) / total_examples}
+
+    """
+    The weighted_average function calculates
+    a weighted average of accuracy metrics from
+    all participating clients, where each client's
+    contribution is proportional to the size of their dataset.
+    """
+
+def on_fit_config(server_round: int) -> Metrics:
+    """Adjusts learning rate based on current round."""
+    lr = 0.01
+    # Appply a simple learning rate decay
+    if server_round > 2:
+        lr = 0.005
+    return {"lr": lr}
+
+
+
+
+
+def server_fn(context: Context):
+    # Read from config
+    num_rounds = context.run_config["num-server-rounds"]
+    fraction_fit = context.run_config["fraction-fit"]
+
+    # Initialize model parameters
+    ndarrays = get_weights(Net())
+    parameters = ndarrays_to_parameters(ndarrays)
+
+    # Define strategy
+    strategy = FedAvg(
+        fraction_fit=fraction_fit,
+        fraction_evaluate=1.0,
+        min_available_clients=2,
+        initial_parameters=parameters,
+        evaluate_metrics_aggregation_fn=weighted_average,
+        on_fit_config_fn=on_fit_config,
+    )
+    config = ServerConfig(num_rounds=num_rounds)
+
+    return ServerAppComponents(strategy=strategy, config=config)
+
+
+# Create ServerApp
+app = ServerApp(server_fn=server_fn)
