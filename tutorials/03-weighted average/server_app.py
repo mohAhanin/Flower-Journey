faf8399df33ff1dd5c8c50ac79dc2b8ea60ec452
unknown <maahaninir@gmail.com>
2025-05-22 23:15:21 +0330

    weighted average (Distributed)

---
+++ tutorials/03-weighted average/server_app.py
@@ -0,0 +1,49 @@
+"""myapp: A Flower / PyTorch app."""
+
+from flwr.common import Context, ndarrays_to_parameters, Metrics
+from flwr.server import ServerApp, ServerAppComponents, ServerConfig
+from flwr.server.strategy import FedAvg
+from myapp.task import Net, get_weights
+from typing import List, Tuple
+
+
+
+def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
+    """Aggregates metrics from an evaluate round."""
+    # Loop trough all metrics received compute accuracies x examples
+    accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
+    total_examples = sum(num_examples for num_examples, _ in metrics)
+    # Return weighted average accuracy
+    return {"accuracy": sum(accuracies) / total_examples}
+
+    """
+    The weighted_average function calculates
+    a weighted average of accuracy metrics from
+    all participating clients, where each client's
+    contribution is proportional to the size of their dataset.
+    """
+
+def server_fn(context: Context):
+    # Read from config
+    num_rounds = context.run_config["num-server-rounds"]
+    fraction_fit = context.run_config["fraction-fit"]
+
+    # Initialize model parameters
+    ndarrays = get_weights(Net())
+    parameters = ndarrays_to_parameters(ndarrays)
+
+    # Define strategy
+    strategy = FedAvg(
+        fraction_fit=fraction_fit,
+        fraction_evaluate=1.0,
+        min_available_clients=2,
+        initial_parameters=parameters,
+        evaluate_metrics_aggregation_fn=weighted_average,
+    )
+    config = ServerConfig(num_rounds=num_rounds)
+
+    return ServerAppComponents(strategy=strategy, config=config)
+
+
+# Create ServerApp
+app = ServerApp(server_fn=server_fn)
